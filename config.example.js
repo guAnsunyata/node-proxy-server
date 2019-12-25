afaa67cb1c76afb9595097993aeee670f510d755
guansunyata <guansunyata@gmail.com>
2019-12-25 13:58:52 +0800

    Update example config for SSL certificate settings

--- config.example.js
+++ config.example.js
@@ -1,10 +1,25 @@
 
 module.exports = {
-  proxyServerProt: 81,
+  proxyHttpsServerProt: 443,
+  proxyHttpServerProt: 80,
   configs: [
     {
-      domain: 'mamilove.com.tw.cat',
-      target: 'http://localhost:8005',
+      domain: 'exampleA.com',
+      target: 'http://localhost:3000',
+    },
+    {
+      domain: '(.*).exampleB.com',
+      target: 'http://localhost:3001',
     },
   ],
+  certificates: {
+    'exampleA.com': {
+      keyPath: './exampleA.com-key.pem',
+      certPath: './exampleA.com-cert.pem',
+    },
+    '(.*).exampleB.com': {
+      keyPath: './exampleB.com-key.pem',
+      certPath: './exampleB.com-cert.pem',
+    },
+  },
 }
